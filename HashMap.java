3556d64ed4dc6f03640e4cafa1c4dbc67d1eb798
Anubhab Chattopadhyay <chattopadhyayanubhab@gmail.com>
2023-08-14 23:39:41 +0530

    Create HashMap.java

---
+++ HashMap.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.*;
+public class HashMap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map=new HashMap<>();
+        map.put("Ram",1);
+        map.put("Shyam",2);
+        map.put("Jadu",3);
+        System.out.println(map);
+    }
+    
+}
